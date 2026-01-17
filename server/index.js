52c19ce2e11655389d521e5f3305578b4b2c0ca6
Adilshah2026 <dil@skilllogic.in>
2026-01-17 10:25:28 +0530

    Backend authentication with profile and middleware completed

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './utils/db.js';
+import userRoute from './routes/User.route.js';
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/users', userRoute);
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`server runing at PORT: ${PORT}`); 
+});
---
+++ server/models/Otp.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const otpSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      required: true,
+      unique:true
+    },
+
+    otp: {
+      type: String,
+      required: true
+    },
+
+   expiresAt: {
+      type: Date,
+      default: () => Date.now() + 5 * 60 * 1000,
+      index: { expires: "5m" }
+    }
+
+},{timestamps:true});
+
+const Otp = mongoose.model("OTP", otpSchema);
+export default Otp;
