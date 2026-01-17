a341d82df4d8aaf969fd1c703bc210867f0dcd2d
Adilshah2026 <dil@skilllogic.in>
2026-01-17 11:02:35 +0530

    Fix auth middleware and profile route

--- server/middlewares/isAuth.middleware.js
+++ server/middlewares/isAuth.middleware.js
@@ -14,7 +14,19 @@ export const isAuth = async (req, res, next) => {
 
     const decodedData = jwt.verify(token, process.env.SECRET_KEY);
 
-    req.user = await User.findById(decodedData.userId);
+    const user = await User.findById(decodedData.userId).select("-password");
+
+     if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+      req.user = user;
+      console.log(user.role);
+      
+
 
     next();
   } catch (error) {
